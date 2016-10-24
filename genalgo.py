df5d1cd724e30d3285d89143cd0e3f4305e5f9a0
John Anderson <jander43@vols.utk.edu>
2016-10-24 16:48:10 -0400

    Refactored code into separate modules for 'JVM' work and 'Genetic Algorithm' work: jvm.py, and genalgo.py

---
+++ genalgo.py
@@ -0,0 +1,12 @@
+'''
+Author: John D. Anderson
+Email: jander43@vols.utk.edu
+Program: genalgo
+Description:
+    Module for implementing genetic algorithm using NetLogo instance running on
+    a Java virtual machine as the fitness function.
+'''
+
+# libraries
+import deap
+import jvm
--- genetlogo.py
+++ genetlogo.py
@@ -9,72 +9,11 @@ Description:
 '''
 
 # libraries
-import subprocess
-import time
-
-# 3rd party libraries
-from py4j.java_gateway import JavaGateway
-import deap
-
-# constants
-PRG = 'INDISIM3Controller'
-TST = (13.0, 12.0, 500.0, 1.80, 50.0, 0.5, 0.0,
-       0.50, 0.25, 200.0, 300.0, 901200.0, 10.0,
-       0.040, 3.00, 200)
-
-
-# classes
-class JVM(object):
-
-    # constructor
-    def __init__(self, prgpath):
-
-        # build command
-        self.cmd = ['java', prgpath]
-
-    # functions
-    def run_java_code(self, params):
-
-        # get fit values
-        fit = self._java_object.fitness_function(*params)
-
-        # print values
-        for val in fit:
-            print val
-
-    # enter method for 'with' statement (see PEP 343)
-    def __enter__(self):
-
-        # start JVM
-        subprocess.os.chdir('java_source/')
-        self._jvm = subprocess.Popen(self.cmd)
-
-        # wait
-        time.sleep(1)
-
-        # startup gateway
-        self._gateway = JavaGateway()
-
-        # get gateway entry point
-        self._java_object = self._gateway.entry_point
-        self.fitness_function = self._java_object.fitness_function
-
-        # finally return self
-        return self
-
-    # exit method for 'with' statement (see PEP 343)
-    def __exit__(self, exception_type, exception_value, traceback):
-
-        # close gateway server
-        self._gateway.shutdown()
-
-        # kill jvm
-        self._jvm.kill()
+import genalgo
 
 
 # executable
 if __name__ == '__main__':
 
     # using with keyword for safe execution
-    with JVM(PRG) as jcode:
-        jcode.run_java_code(TST)
+    print 'under construction'
