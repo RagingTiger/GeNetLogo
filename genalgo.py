d36a50bb21d78407447fe8a7a13639ca2341b76a
John Anderson <jander43@vols.utk.edu>
2016-10-27 11:17:17 -0400

    Added code to create types and register functions

--- genalgo.py
+++ genalgo.py
@@ -8,5 +8,32 @@ Description:
 '''
 
 # libraries
+import random
 import deap
 import jvm
+
+# creating simulations individual
+deap.creator.create('FitMax', deap.base.Fitness, weights=(1.0,))
+deap.creator.create('SimulationCell', list, fitness=creator.FitMax,
+                    params=dict)
+
+# crate toolbox
+toolbox = deap.base.Toolbox()
+
+# register functions
+toolbox.register('attr_bool', random.randint, 0, 1)
+toolbox.register('simcell', deap.tools.initRepeat, deap.creator.SimulationCell,
+                 toolbox.attr_bool, 100)
+toolbox.register('population', deap.tools.initRepeat, list, toolbox.simcell)
+
+
+# define evaluation function
+def eval_fit(individual):
+    return sum(individual)
+
+
+# executable
+if __name__ == '__main__':
+
+    # print
+    print 'under construction'
