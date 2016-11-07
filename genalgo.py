b09be285877398f1dfe6d83681b273a041889960
John Anderson <jander43@vols.utk.edu>
2016-11-07 10:24:27 -0500

    Added 'main()' function and refactored to work with the wonderful 'docopt' library: docopt.org

--- genalgo.py
+++ genalgo.py
@@ -3,10 +3,12 @@
 '''
 Author: John D. Anderson
 Email: jander43@vols.utk.edu
-Program: genalgo
 Description:
     Module for implementing genetic algorithm using NetLogo instance running on
     a Java virtual machine as the fitness function.
+Documentation: http://deap.readthedocs.io/en/master/
+Usage:
+    genetalgo <population>
 '''
 
 # libraries
@@ -33,9 +35,30 @@ toolbox.register('population', tools.initRepeat, list, toolbox.simcell)
 def eval_fit(individual):
     return sum(individual)
 
+# register genetic operations
+toolbox.register("evaluate", eval_fit)
+toolbox.register("mate", tools.cxTwoPoint)
+toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
+toolbox.register("select", tools.selTournament, tournsize=3)
+
+
+def main(pval):
+    '''
+    Function to run Genetic Algorithm.
+    '''
+    # initial population
+    pop = toolbox.population(n=int(pval))
+
 
 # executable
 if __name__ == '__main__':
 
+    # exectuable only imports
+    from docopt import docopt
+
+    # check CLA
+    args = docopt(__doc__)
+
     # print
     print 'Starting Genetic Algorithm ...'
+    main(args['<population>'])
