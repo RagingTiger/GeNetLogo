3ca9dec1c990dc64fbb07df1f821fa46fd98fe21
John Anderson <jander43@vols.utk.edu>
2016-11-11 12:31:04 -0500

    Filled out 'GeNetLogo' class

--- genetlogo.py
+++ genetlogo.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 '''
 Author: John D. Anderson
 Email: jander43@vols.utk.edu
@@ -6,10 +7,13 @@ Program: genetlogo
 Description:
     This program searches a combination space of NetLogo simulation parameters
     in an effort to find the maximum/minimum value.
+Usage:
+    genetlogo run <NetLogoJavaController>
+    genetlogo test
 '''
 
 # libraries
-# import genalgo
+import genalgo
 import jvm
 
 # banner for stdout
@@ -26,32 +30,45 @@ banner = '''
 
 
 # classes
-class GeNetLogo(object):
+class GeNetLogo(genalgo.GenAlgo):
     '''
     Class to apply Genetic Algorithms to NetLogo simulations.
     '''
     # constructor
-    def __init__(self):
-        print 'under construction'
+    def __init__(self, funcname, func):
+        # call base class constructor
+        GenAlgo.__init__(self, pops, gens, funcname, func, 'self.eval_fit')
+
+    # fitness evaluation function
+    def eval_fit(self, individual):
+        '''
+        Function to evaluate fitness of individuals for GeNetLogo applications.
+        '''
+        return sum(individual),
 
 
 # executable
 if __name__ == '__main__':
 
+    # executabel import only
+    from docopt import docopt
+
     # print
-    print '{0}'.format(banner)
+    print banner
+
+    # check CLA
+    args = docopt(__doc__)
+
+    # control flow
+    if args['test']:
+        java_controller = jvm.PRG
+
+    elif args['run']:
+        java_controller = args['<NetLogoJavaController>']
 
     # start JVM
     print 'Starting Java Virtual Machine ...'
-    with jvm.JVM(jvm.PRG) as jcode:
+    with jvm.JVM(java_controller) as jcode:
 
-        # ready to rock n roll
-        print 'Virtual Machine Started'
-
-        # get results
-        results = jcode.run_java_code(jvm.TST)
-        print 'Results:'
-        for val in results:
-            print val
-
-    # print 'Starting Genetic Algorithm ...'
+        # launch Genetic Algorithm
+        print 'Virtual Machine Started!\nStarting Genetic Algorithm ...'
